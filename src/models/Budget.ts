17fdc0054672cadbf3867a94fef0800b0b777c32
Bob Correa <bob.correa@oclock.school>
2025-04-14 23:49:32 +0200

    add CRUD to BudgetModel

--- src/models/Budget.ts
+++ src/models/Budget.ts
@@ -21,5 +21,27 @@ const BudgetModel = {
         return result.rows[0];
     },
     
+    async create(budget: BudgetObject) {
+        const query = "INSERT INTO budget (user_id, category, limit) VALUES ($1, $2, $3) returning *";
+        const values = [budget.userId, budget.category, budget.limit];
+        const result = await db.query(query, values);
+        return result.rows[0];
+    },
+
+    async update(id: number, updates: Partial<BudgetObject>) {
+        const query =
+            "UPDATE budgets SET category = COALESCE($1, category), limit = COALESCE($2, limit) WHERE id = $3 RETURNING *";
+        const values = [updates.category, updates.limit, id];
+        const result = await db.query(query, values);
+        return result.rows[0];
+    },
+
+    async delete(id: number) {
+        const query = "DELETE FROM budgets WHERE id = $1 RETURNING *";
+        const result = await db.query(query, [id]);
+        return result.rowCount > 0;
+    },
+
+    };
     
-}
+export {BudgetModel}
