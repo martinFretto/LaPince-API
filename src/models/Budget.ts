7da14452c191e58b3e49da434f1d7e32514fc88c
Bob Correa <bob.correa@oclock.school>
2025-04-14 23:39:26 +0200

    add second query

--- src/models/Budget.ts
+++ src/models/Budget.ts
@@ -14,5 +14,12 @@ const BudgetModel = {
         const result = await db.query(query, [userId]);
         return result.rows;
     },
+
+    async findById(id:number) {
+        const query = "SELECT * FROM budget WHERE id = $1";
+        const result = await db.query(query, [id]);
+        return result.rows[0];
+    },
+    
     
 }
