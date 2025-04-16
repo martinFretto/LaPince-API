da1b28e68935c2fe19fc75b36c5fa26eb7209542
Bob Correa <bob.correa@oclock.school>
2025-04-16 18:06:35 +0200

    Updated budgetController

--- src/controllers/budgetController.ts
+++ src/controllers/budgetController.ts
@@ -2,39 +2,40 @@ import { Request, Response } from "express";
 import { Budget } from "../models/Budget";
 import { BudgetObject } from "../types/ModelTypes";
 import jwt from "jsonwebtoken";
+import Joi from "joi";
+import { getUserIdInToken } from "../libs/jwtToken";
 
 
 interface AuthenticatedRequest extends Request {
     token?: string;
   }
 
+  // Schéma de validation Joi pour les budgets
+const budgetSchema = Joi.object({
+    name: Joi.string().max(255).required(),
+    warning_amount: Joi.number().min(0).required(),
+    spent_amount: Joi.number().min(0).optional(),
+    allocated_amount: Joi.number().min(0).required(),
+    color: Joi.string().max(255).optional(),
+    icon: Joi.string().max(255).optional(),
+});
 
 async function getAllBudgets(req: AuthenticatedRequest, res: Response): Promise<void> {
     try {
-        // On récupère l'Id depuis les paramètre de la requete puis on l'affiche dans les logs
-        const { userId } = req.params;
-        console.log("USERID:", userId);
-
-        // Decodage du token avec une clé secrète
-        const decoded = jwt.verify
-
-        ("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImVtYWlsIjoiYm9iYnlAb2Nsb2NrLmlvIiwiaWQiOjJ9LCJpYXQiOjE3NDQ3MTc2OTMsImV4cCI6MTc0NDcxODg5M30.tk0pA6wwWRqe5Ax6DL0RXA3eXwJ7-zXoybeU289aEo4",
-            process.env.JWT_SECRET as jwt.Secret) as jwt.JwtPayload;
-
-            //Extraire l'id utilisateur du token décodé puis l'affiché dans les logs
-        const user_id = decoded.user.id;
-        console.log("user_id???: ", user_id)
-        const token=""
-
-        //On convertit l'id utilisateur en type number puis on verifie avec les logs
-        const user_id_for_db= parseInt(user_id);
-
-        console.log("user_id_for_db: ", user_id_for_db, "  type: ", typeof user_id_for_db)
+        
+       //On récupère l'id de l'utilisateur dans le token
+        const user_id_for_db = getUserIdInToken(req);
         
 
+       
+
         // Requête pour obtenir tous les budgets liés à cet utilisateur
-        const budgets = await Budget.prototype.findByUserId(parseInt(userId));
-        res.status(200).json({ status: 200, data: budgets });
+        const budgets = await BudgetDatamapper.findByUserId(user_id_for_db);
+        if (budgets?.length) {
+            res.status(200).json({ status: 200, data: budgets });
+        } else {
+            res.status(404).json({ status: 404, message: "Aucun budget trouvé pour cet utilisateur." });
+        }
     } catch (error) {
         //Capture des erreurs
         console.error("Erreur dans getAllBudgets :", error);
@@ -46,16 +47,19 @@ async function getBudgetById(req: AuthenticatedRequest, res: Response): Promise<
     try {
         // On récupère l'id depuis les paramètre de la requete puis on l'affiche dans les logs
         const { id } = req.params;
-        console.log("ID:",id);
-        //Requete pour rechercher un budget selon son id
-        const budget = await Budget.prototype.findById(parseInt(id));
+        const user_id_for_db = getUserIdInToken(req)
 
-        if (!budget) {
-            // Si aucun budget n'est trouvé retourné une reponse avec un message.
-            res.status(404).json({ status: 404, message: "Budget introuvable." });
+        console.log("ID:",id, "Token:", user_id_for_db);
+        //Requete pour rechercher un budget selon son id
+
+        const budget = await BudgetDatamapper.findById(id, user_id_for_db);
+
+            //Si le budget existe il le retourne sinon on retourne un message d'erreur
+        if (budget) {
+            res.status(200).json({ status: 200, data: budget });
+        } else {
+            res.status(404).json({ status: 404, message: "Ce budget est introuvable." });
         }
-        // Retourne le budget trouvé
-        res.status(200).json({ status: 200, data: budget });
     } catch (error) {
         //Capture les erreurs
         console.error("Erreur dans getBudgetById :", error);
@@ -65,30 +69,35 @@ async function getBudgetById(req: AuthenticatedRequest, res: Response): Promise<
 
 async function createBudget(req: AuthenticatedRequest, res: Response): Promise<void> {
     try {
+        const user_id_for_db = getUserIdInToken(req);
         const { name, warning_amount, spent_amount, allocated_amount, color, icon, user_id } = req.body;
 
-        // Validation de la présence des champs obligatoires
-        if (!name || !allocated_amount || !user_id) {
+        // Valider les données avec Joi
+        const { error } = budgetSchema.validate(req.body);
+        if (error) {
             res.status(400).json({
-                status: 400,
-                message: "Les champs name, allocated_amount, et user_id sont obligatoires.",
+                message: "Validation échouée !",
+                details: error.details.map((detail) => detail.message),
             });
-            //Arrete l'execution si les champs obligatoire sont absent
             return;
         }
         // Crée un objet de Budget avec donnée 
         const newBudget: BudgetObject = {
             name,
-            warning_amount: parseFloat(warning_amount) || 0,
-            spent_amount: parseFloat(spent_amount) || 0,
-            allocated_amount: parseFloat(allocated_amount),
+            warning_amount: Number(warning_amount) || 0,
+            spent_amount: Number(spent_amount) || 0,
+            allocated_amount: Number(allocated_amount),
             color: color || null,
             icon: icon || null,
-            user_id: parseInt(user_id),
+            user_id: Number(user_id),
         };
         //Inserer le budget dans la base de donnée
-        const createdBudget = await Budget.prototype.create(newBudget);
-        res.status(201).json({ status: 201, message: "Budget créé avec succès.", data: createdBudget });
+        const createdBudget = await BudgetDatamapper.create(newBudget);
+        if (createdBudget) {
+            res.status(201).json({ status: 201, message: "Budget créé avec succès.", data: createdBudget });
+        } else {
+            res.status(500).json({ status: 500, message: "Une erreur est survenue lors de la création du budget." });
+        }
     } catch (error) {
         // Capture les erreurs
         console.error("Erreur dans createBudget :", error);
@@ -96,38 +105,51 @@ async function createBudget(req: AuthenticatedRequest, res: Response): Promise<v
     }
 }
 
-async function updateBudget(req: Request, res: Response): Promise<void> {
+async function updateBudget(req: AuthenticatedRequest, res: Response): Promise<void> {
     try {
         //Recupere l'id et les données à mettre a jour
-        const { id } = req.params;
-        const { name, warning_amount, spent_amount, allocated_amount, color, icon } = req.body;
 
-        //Valide qu'au moins un champs est fourni pour la mise a jour
-        if (!name && !warning_amount && !spent_amount && !allocated_amount && !color && !icon) {
-            res.status(400).json({
-                status: 400,
-                message: "Veuillez fournir au moins un champ à mettre à jour.",
-            });
+        const { id } = req.params;
+        const user_id_for_db = getUserIdInToken(req);
+
+        const budget = await BudgetDatamapper.findById(id, user_id_for_db);
+
+        if (!budget) {
+            res.status(404).json({ status: 404, message: "Ce budget est introuvable." });
             return;
         }
+
+        const { name, warning_amount, spent_amount, allocated_amount, color, icon } = req.body;
+
         // Crée un objet partiel avec les donnée a mettre a jour
         const updates: Partial<BudgetObject> = {
             name,
-            warning_amount: warning_amount ? parseFloat(warning_amount) : undefined,
-            spent_amount: spent_amount ? parseFloat(spent_amount) : undefined,
-            allocated_amount: allocated_amount ? parseFloat(allocated_amount) : undefined,
+            warning_amount: warning_amount ? Number(warning_amount) : undefined,
+            spent_amount: spent_amount ? Number(spent_amount) : undefined,
+            allocated_amount: allocated_amount ? Number(allocated_amount) : undefined,
             color,
             icon,
         };
-        //Utilise le modèle pour effectuer la mise a jour dans la BDD
-        const updatedBudget = await Budget.prototype.update(parseInt(id), updates);
 
-        if (!updatedBudget) {
-            res.status(404).json({ status: 404, message: "Budget introuvable." });
-        }
+       // Valider les données avec Joi
+       const { error } = budgetSchema.validate(updates, { allowUnknown: true });
+       if (error) {
+           res.status(400).json({
+               message: "Validation échouée !",
+               details: error.details.map((detail) => detail.message),
+           });
+           return;
+       }
     
-        //retourne le budget mis a jour
-        res.status(200).json({ status: 200, message: "Budget mis à jour avec succès.", data: updatedBudget });
+        
+        //Utilise le modèle pour effectuer la mise a jour dans la BDD
+        const updatedBudget = await BudgetDatamapper.update(id, updates);
+
+        if (updatedBudget) {
+            res.status(200).json({ status: 200, message: "Budget mis à jour avec succès.", data: updatedBudget });
+        } else {
+            res.status(500).json({ status: 500, message: "Une erreur est survenue lors de la modification du budget." });
+        }    
     } catch (error) {
         console.error("Erreur dans updateBudget :", error);
         res.status(500).json({ status: 500, message: "Une erreur serveur s'est produite." });
@@ -135,26 +157,25 @@ async function updateBudget(req: Request, res: Response): Promise<void> {
     return;
 }
 
-async function deleteBudget(req: Request, res: Response): Promise<void> {
+async function deleteBudget(req: AuthenticatedRequest, res: Response): Promise<void> {
     try {
         // On recupere l'd du buget a supprimer
         const { id } = req.params;
+        const user_id_for_db = getUserIdInToken(req);
         // On verifie que le budget a supprimer existe bien avent de le supprimer
 
-        // Supprime le budget de la BDD
-        const deleted = await Budget.prototype.delete(parseInt(id));
+        const budget = await BudgetDatamapper.findById(id, user_id_for_db);
 
-        if (!deleted) {
-            res.status(404).json({ status: 404, message: "Budget introuvable." });
+        if (budget) {
+            await BudgetDatamapper.delete(id);
+            res.status(204).json({ status: 204, message: "Budget supprimé avec succès." });
+        } else {
+            res.status(404).json({ status: 404, message: "Le budget que vous souhaitez supprimer n'existe pas." });
         }
-        //Retourne une confirmation 
-        res.status(200).json({ status: 200, message: "Budget supprimé avec succès." });
     } catch (error) {
-        // Capture des erreurs
         console.error("Erreur dans deleteBudget :", error);
         res.status(500).json({ status: 500, message: "Une erreur serveur s'est produite." });
     }
-    return;
 }
 
 export { getAllBudgets, getBudgetById, createBudget, updateBudget, deleteBudget };
