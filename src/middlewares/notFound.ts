19ead218d9f06ae01d4960fec65f566e4ef4b57c
martinFretto <martin.fretto@gmail.com>
2025-04-17 15:38:11 +0200

    correction 400->404

--- src/middlewares/notFound.ts
+++ src/middlewares/notFound.ts
@@ -2,6 +2,6 @@ import { NextFunction, Request, Response } from "express";
 
 
 export const notFound = (req: Request, res: Response, next: NextFunction) => {
-    res.status(400).json({ status: 400, message: "Vous cherchez une page inexistante" });
+    res.status(404).json({ status: 404, message: "Vous cherchez une page inexistante" });
     return;
 }
