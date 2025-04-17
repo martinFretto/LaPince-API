4e622ed925de5b0d9ddd645693b259d3ee987084
martinFretto <martin.fretto@gmail.com>
2025-04-17 15:33:31 +0200

    not found middleware

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,18 @@ import express from 'express';
 import 'dotenv/config';
 import { router } from './router';
 import helmet from 'helmet';
+import { notFound } from './middlewares/notFound';
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(helmet());
 app.use(express.json());
 app.use(router);
 
-app.use(helmet());
+app.use(notFound);
+
+
 
 app.listen(PORT, () =>{
     console.log(`Server listening on port ${PORT}`);
