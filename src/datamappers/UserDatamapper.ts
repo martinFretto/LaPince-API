8f67d9e289922074272aefb43eca27944cd0f8b5
martinFretto <martin.fretto@gmail.com>
2025-04-18 14:15:35 +0200

    console log on registerUser to test deploy

--- src/datamappers/UserDatamapper.ts
+++ src/datamappers/UserDatamapper.ts
@@ -5,14 +5,13 @@ import { UserObject } from "../types/ModelTypes";
 // biome-ignore lint/complexity/noStaticOnlyClass: <explanation>
 class UserDatamapper {
     static async findByEmail(email: string): Promise<null|User> {
-        console.log("findByEmail: ")
         const query = {
             text: 'SELECT * FROM "user" WHERE email = $1',
             values: [email],
         };
         console.log("DB: ", db)
         const results = await db.query(query);
-
+        console.log("results: ", results)
 
         if (!results.rowCount) {
             return null;
