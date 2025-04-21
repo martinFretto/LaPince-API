87620ba9e8ebfb4a4df316160b56b016542b7086
Bob Correa <bob.correa@oclock.school>
2025-04-21 20:07:34 +0200

    Modif db.ts et .env itegration database_url de render

--- src/database/db.ts
+++ src/database/db.ts
@@ -5,12 +5,18 @@ dotenv.config();
 
 // Configuration du pool de connexions
 const db = new Pool({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database:  process.env.PG_NAME,
-    password:  process.env.PG_PASSWORD,
-    port: Number(process.env.PG_PORT || "5432"), 
-  });
-  
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database:  process.env.PG_NAME,
+  password:  process.env.PG_PASSWORD,
+  port: parseInt(process.env.PG_PORT || "5432"), 
+});
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+      rejectUnauthorized: false, // Nécessaire pour Render
+  },
+});
 
   export {db}
