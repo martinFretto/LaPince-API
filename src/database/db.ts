52828af3eeb7fa0f2b1847a0b226e8aa186d46e5
martinFretto <martin.fretto@gmail.com>
2025-04-22 16:50:53 +0200

    db

--- src/database/db.ts
+++ src/database/db.ts
@@ -5,11 +5,12 @@ dotenv.config();
 
 // Configuration du pool de connexions
 const db = new Pool({
-    user: process.env.PG_USER,
+    connectionString: process.env.DATABASE_URL,
+   /* user: process.env.PG_USER,
     host: process.env.PG_HOST,
     database:  process.env.PG_NAME,
     password:  process.env.PG_PASSWORD,
-    port: parseInt(process.env.PG_PORT || "5432"), 
+    port: parseInt(process.env.PG_PORT || "5432"), */
     ssl: {
 
       rejectUnauthorized: false, // Nécessaire pour Render
