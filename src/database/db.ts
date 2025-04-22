e1cad3ec7462631bdbca83ebf5cfb686adce8ab0
Bob Correa <bob.correa@oclock.school>
2025-04-22 10:40:06 +0200

    Mdif db.ts pour deploiement

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,22 +1,16 @@
-import {Pool} from 'pg';
+import { Pool } from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-// Configuration du pool de connexions
-const db = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database:  process.env.PG_NAME,
-  password:  process.env.PG_PASSWORD,
-  port: parseInt(process.env.PG_PORT || "5432"), 
-});
-
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-      rejectUnauthorized: false, // Nécessaire pour Render
-  },
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false, // Nécessaire pour Render
+    },
 });
 
-  export {db}
+export default pool;
+
+
+  export {pool}
