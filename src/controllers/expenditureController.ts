0fbc58569e4ca8b172f50597bf9015fbd0926654
Bob Correa <bob.correa@oclock.school>
2025-04-24 18:03:57 +0200

    commit for last merge

--- src/controllers/expenditureController.ts
+++ src/controllers/expenditureController.ts
@@ -17,13 +17,7 @@ export async function getAllExpenditures(req: AuthenticatedRequest, res: Respons
     //On récupère l'id de l'utilisateur dans le token
     const user_id_for_db = getUserIdInToken(req);
     
-<<<<<<< HEAD
-    
-    
-            let expenditures; 
-=======
     let expenditures; 
->>>>>>> 694ba15158842d34226b8a9e2a22bd841fccf4dd
 
     if(budgetId){
         const budget_id_for_db = Number(budgetId)
@@ -47,10 +41,6 @@ export async function getOneExpenditure(req: AuthenticatedRequest, res: Response
      //on convertit ce qui doit être convertit 
     //Ce qui vient du token est de la route est au format string, on veut des number 
      const expenditure_id_for_db = Number(expenditure_id)
-<<<<<<< HEAD
-    
-=======
->>>>>>> 694ba15158842d34226b8a9e2a22bd841fccf4dd
 
     //On récupère l'id de l'utilisateur dans le token
     const user_id_for_db = getUserIdInToken(req);
